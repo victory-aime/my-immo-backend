7cc56b856f610d37740a7dba70a1a913679ff60b
amina78606 <aminatadiba10@gmail.com>
2026-05-11 09:55:06 +0000

    feat(stats): create stats module

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,7 @@ import { AnnonceModule } from '_root/modules/annonce/annonce.module';
 import { TeamModule } from '_root/modules/team/team.module';
 import { LeadsModule } from '_root/modules/leads/leads.module';
 import { VisitsModule } from '_root/modules/visits/visits.module';
+import { StatsModule } from '_root/modules/stats/stats.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { NotificationsModule } from './modules/notifications/notifications.module';
 
@@ -67,6 +68,7 @@ import { NotificationsModule } from './modules/notifications/notifications.modul
     TeamModule,
     LeadsModule,
     VisitsModule,
+    StatsModule,
     NotificationsModule,
   ],
 
