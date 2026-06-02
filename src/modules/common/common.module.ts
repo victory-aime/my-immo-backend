c270293ff14a63722b54c29d584d8d17b6ad77a8
victory-aime <victorydarnelmbenze@gmail.com>
2026-06-03 00:44:27 +0100

    fix(firebase): firebase config
    update firebase config change const in service and update Test push controller

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -8,6 +8,7 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 import { SubscriptionLimitService } from '_root/modules/common/services/subscription-limit.service';
+import { FirebaseService } from '_root/modules/common/services/firebase-admin.service';
 
 @Module({
   imports: [
@@ -22,7 +23,14 @@ import { SubscriptionLimitService } from '_root/modules/common/services/subscrip
     PaymentService,
     NabooService,
     SubscriptionLimitService,
+    FirebaseService,
   ],
-  exports: [PermissionsService, PaymentService, NabooService, SubscriptionLimitService],
+  exports: [
+    PermissionsService,
+    PaymentService,
+    NabooService,
+    SubscriptionLimitService,
+    FirebaseService,
+  ],
 })
 export class CommonModule {}
