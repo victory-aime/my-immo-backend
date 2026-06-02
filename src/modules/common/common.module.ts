5143ba23a20239502d92a07e30578d65696d0917
victory-aime <victorydarnelmbenze@gmail.com>
2026-06-02 15:07:59 +0100

    refactor(docs): correct docs
    get all changes and fix rebase for docs

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -2,12 +2,27 @@ import { Module } from '@nestjs/common';
 import { PermissionsService } from './services/permissions.service';
 import { CommonController } from './common.controller';
 import { CommonService } from '_root/modules/common/common.service';
-import { SubscriptionLimitService } from './services/subscription-limit.service';
+import { PaymentService } from '_root/modules/common/services/payment.service';
+import { NabooService } from '_root/modules/common/services/naboo.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
+import { SubscriptionLimitService } from '_root/modules/common/services/subscription-limit.service';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule.register({ timeout: 15_000, maxRedirects: 3 }),
+    ConfigModule,
+    CloudinaryModule,
+  ],
   controllers: [CommonController],
-  providers: [PermissionsService, CommonService, SubscriptionLimitService], // ✅
-  exports: [PermissionsService, SubscriptionLimitService], // ✅
+  providers: [
+    PermissionsService,
+    CommonService,
+    PaymentService,
+    NabooService,
+    SubscriptionLimitService,
+  ],
+  exports: [PermissionsService, PaymentService, NabooService],
 })
 export class CommonModule {}
