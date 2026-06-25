c9799172719c7e4350788a834edb4a03a97af91a
Amina78606 <aminatadiba10@gmail.com>
2026-06-25 23:41:25 +0000

    feat(plans): implement subscription plans CRUD and features management (#17)
    -Récupération de tous les plans
    -Récupération des détails d'un plan par ID.
    Création d'un plan
    -Mise à jour d'un plan
    -Activation/Désactivation d'un plan.
    -Suppression sécurisée d'un plan (bloquée si des agences y sont abonnées).
    -Documentation Swagger complète
    
    Co-authored-by: victory-aime <victorydarnelmbenze@gmail.com>

--- src/modules/packs/pack.module.ts
+++ src/modules/packs/pack.module.ts
@@ -2,11 +2,13 @@ import { PackService } from '_root/modules/packs/pack.service';
 import { PackController } from '_root/modules/packs/pack.controller';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '_root/database/database.module';
+import { PackAdminService } from './pack-admin.service';
+import { AdminPackController } from './admin-pack.controller';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [PackController],
-  providers: [PackService],
+  controllers: [PackController, AdminPackController],
+  providers: [PackService, PackAdminService],
   exports: [PackService],
 })
 export class PackModule {}
